12ee808209ff45d55d0e1a463f8a7067f3195fc7
newbureau <test.newbureau@gmail.com>
2023-01-29 15:27:02 +0700

    add homework_02

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,18 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class LowFuelError(Exception):
+    """ Exception LowFuelError"""
+    pass
+
+
+class NotEnoughFuel(Exception):
+    """ Exception NotEnoughFuel"""
+    pass
+
+
+class CargoOverload(Exception):
+    """ Exception CargoOverload"""
+    pass
