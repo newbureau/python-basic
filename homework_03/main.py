053142e8899a22af0a6bf94b5e88d3d15e856983
newbureau <test.newbureau@gmail.com>
2023-02-12 20:18:44 +0700

    remake homework_03

--- homework_03/main.py
+++ homework_03/main.py
@@ -6,7 +6,6 @@ app = FastAPI()
 
 @app.get("/ping/")
 def read_ping():
-    # return json.dumps({"hello": "root"})
     return Response(
         content=json.dumps({"message": "pong"}),
         media_type="application/json",
