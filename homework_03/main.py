66c9844ff5f7a4bde6787225a95b5525dbb22466
newbureau <test.newbureau@gmail.com>
2023-02-12 20:03:43 +0700

    add homework_03

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,3 +1,13 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI, Response
 
 app = FastAPI()
+
+
+@app.get("/ping/")
+def read_ping():
+    # return json.dumps({"hello": "root"})
+    return Response(
+        content=json.dumps({"message": "pong"}),
+        media_type="application/json",
+    )
